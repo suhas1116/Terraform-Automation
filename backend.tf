661343b614051bf94cf0b66191c9dae1629f65f0
suhas <suhas.dhavane111@gmail.com>
2023-04-13 20:19:03 +0530

    chnage

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-6"
+    bucket = "my-s3bucket-suhas123"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
